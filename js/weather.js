af8a9036c25682cde979165c5c0a144ddef99e84
zsm0129 <105777338+zsm0129@users.noreply.github.com>
2022-07-18 00:58:06 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "8890a94c1f25aedd8dd061c34e8a60c3";
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
